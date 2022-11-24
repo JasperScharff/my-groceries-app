105dce0feeaec131e2aea2dfba3fac2d543b0a6a
Jasper Scharff <jas-scharff@live.nl>
2022-11-24 15:25:46 +0100

    Update

---
+++ src/components/GroceryList.js
@@ -0,0 +1,15 @@
+import InputField from "./InputField.js";
+import List from "./List.js";
+
+function GroceryList({ items, onItemClick, onItemAdd }) {
+  return (
+    <div className="grocery-list">
+      <h2>Shopping List</h2>
+      <InputField onSubmit={onItemAdd} />
+      <List items={items} onItemClick={onItemClick}  />
+
+    </div>
+  );
+}
+
+export default GroceryList;
